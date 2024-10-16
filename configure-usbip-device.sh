72811ca90376be6e07eda4c691eb4ad4dc2ec6d3
Alexandre April <aapril@addenergie.com>
2024-10-16 09:11:30 -0400

    Add erreur if not found

--- configure-usbip-device.sh
+++ configure-usbip-device.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -e
 if [[ -z "${USB_BUS_ID}" ]]; then
-    BUS_ID=`/usr/sbin/usbip list -p -l | grep -i "^busid=${USB_BUS_ID}#" | cut '-d#' -f1 | sed 's/busid=//g'`
-else
     BUS_ID=`/usr/sbin/usbip list -p -l | grep -i "#usbid=${USB_IDVENDOR}:${USB_IDPRODUCT}#" | cut '-d#' -f1`
+else
+    BUS_ID=`/usr/sbin/usbip list -p -l | grep -i "^busid=${USB_BUS_ID}#" | cut '-d#' -f1 | sed 's/busid=//g'`
 fi
-[[ -z "$BUS_ID" ]] || /usr/sbin/usbip bind --$BUS_ID
+
+if [[ -z "${BUS_ID}" ]]; then
+    echo "No device found to configure"
+    exit 1
+fi
+
+/usr/sbin/usbip bind --$BUS_ID
 
--- remove-usbip-device.sh
+++ remove-usbip-device.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 if [[ -z "${USB_BUS_ID}" ]]; then
-    BUS_ID=`/usr/sbin/usbip list -p -l | grep -i "^busid=${USB_BUS_ID}#" | cut '-d#' -f1 | sed 's/busid=//g'`
-else
     BUS_ID=`/usr/sbin/usbip list -p -l | grep -i "#usbid=${USB_IDVENDOR}:${USB_IDPRODUCT}#" | cut '-d#' -f1`
+else
+    BUS_ID=`/usr/sbin/usbip list -p -l | grep -i "^busid=${USB_BUS_ID}#" | cut '-d#' -f1 | sed 's/busid=//g'`
 fi
-[[ -z "$BUS_ID" ]] || /usr/sbin/usbip unbind --$BUS_ID
 
+if [[ -z "${BUS_ID}" ]]; then
+    echo "No device found to remove"
+    exit 1
+fi
+
+/usr/sbin/usbip unbind --$BUS_ID
