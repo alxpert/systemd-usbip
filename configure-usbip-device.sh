56fe18b6670c722370edf5dd5cfa45ef3f64dca9
Alexandre April <aapril@addenergie.com>
2024-10-16 09:19:53 -0400

    Add debug print

--- configure-usbip-device.sh
+++ configure-usbip-device.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 if [[ -z "${USB_BUS_ID}" ]]; then
+    echo "Using USB_IDVENDOR and USB_IDPRODUCT -> ${USB_IDVENDOR}:${USB_IDPRODUCT}"
     BUS_ID=`/usr/sbin/usbip list -p -l | grep -i "#usbid=${USB_IDVENDOR}:${USB_IDPRODUCT}#" | cut '-d#' -f1`
 else
+    echo "Using USB_BUS_ID -> ${USB_BUS_ID}"
     BUS_ID=`/usr/sbin/usbip list -p -l | grep -i "^busid=${USB_BUS_ID}#" | cut '-d#' -f1 | sed 's/busid=//g'`
 fi
 
